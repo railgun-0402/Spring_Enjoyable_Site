a7b650950332e8c05df14165b14d001b0a0882d7
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-08-18 01:01:59 +0900

    create_security_config

---
+++ src/main/java/com/su/enjoy/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.su.enjoy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        // 認証
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/").permitAll()
+        );
+
+        return http.build();
+    }
+
+}
--- src/main/java/com/su/enjoy/domain/controller/top.java
+++ src/main/java/com/su/enjoy/domain/controller/top.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class top {
-    @GetMapping("")
+    @GetMapping("/")
     public String index() {
         return "index";
     }
