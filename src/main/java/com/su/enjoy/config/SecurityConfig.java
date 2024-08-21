8de1f66822deb609faabd03fffb17d76ef09da33
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-08-22 01:06:29 +0900

    create_sample_entity

--- src/main/java/com/su/enjoy/config/SecurityConfig.java
+++ src/main/java/com/su/enjoy/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 @Configuration
 @EnableWebSecurity
@@ -14,12 +18,36 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        // cors設定
+        http.cors((cors) -> cors
+                .configurationSource(this.corsConfigurationSource())
+        );
+        http
+                .csrf((csrf) -> csrf
+                        .ignoringRequestMatchers("/sample")
+                );
         // 認証
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers("/").permitAll()
+                .requestMatchers("/sample").permitAll()
         );
 
+        // csrfを無効にしておく
+        // またCookieを利用してcsrf対策を行う
+        http.csrf((csrf) -> csrf
+                .ignoringRequestMatchers("/api/**"));
         return http.build();
     }
 
+    private CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
+        corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
+        corsConfiguration.addExposedHeader("X-AUTH-TOKEN");
+        corsConfiguration.addAllowedOrigin("http://localhost:8081");
+        corsConfiguration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
+        corsSource.registerCorsConfiguration("/**", corsConfiguration);
+        return corsSource;
+    }
 }
